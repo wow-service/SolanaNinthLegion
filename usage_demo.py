b22feb8bc5fef2b70ec1f533445a92f687c3551e
wow-service <gaoxu_cerber@126.com>
2024-10-20 23:01:01 +0800

    rename usage demo

--- usage_demo.py
+++ usage_demo.py
@@ -4,7 +4,9 @@ from Keypair.core import new
 from Tools.core import ClusterClient
 
 if __name__ == '__main__':
-    account = new()
+    # account = new()
+    # pretty_account = json.dumps(account, indent=4)
+    # print(pretty_account)
 
     address = 'D27DgiipBR5dRdij2L6NQ27xwyiLK5Q2DsEM5ML5EuLK'
     endpoint = 'https://mainnet.helius-rpc.com/?api-key=be2d273e-e71f-4cd7-b7a7-81fed6d0a7c0'
@@ -12,9 +14,6 @@ if __name__ == '__main__':
     values = cluster_cli.get_balance(address_string=address)
 
     token_accounts = cluster_cli.get_token_accounts_by_owner(address_string=address)
-
-    pretty_account = json.dumps(account, indent=4)
-    print(pretty_account)
 
     pretty_value = json.dumps(values, indent=4)
     print(pretty_value)
